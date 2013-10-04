c80a3106c61808abb3eed2f32b161780dc75d4a5
ccomp <cyron.completo@gmail.com>
2013-10-04 08:26:05 -0600

    Update Bingo.java

--- Bingo.java
+++ Bingo.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 
 public class Bingo {
-	
+
 	public static boolean bingo(int[][] board, int[] drawn) {
 		Arrays.sort(drawn);
 
 		int placeholder = 0;
 		int boardholder = 0;
 		int extra = board.length;
+		int score = drawn.length;
 
 		for (int i = 0; i<drawn.length; i++) {
 			for (int c = 0; c<board.length; c++) {
@@ -16,9 +17,7 @@ public class Bingo {
 				if (boardholder == placeholder) {
 					return true;
 				}
-				if (boardholder != extra) {
-					return false;
-				}
+				
 			}
 		}
 
